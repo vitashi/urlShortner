c050e953d42e03c4379fadb8a1bcb6791b8b02df
vitashi <vshisoka@gmail.com>
2022-08-29 14:38:24 +0300

    added src directory; waiting for logic.

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port = 3050
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`We have lift off!! See you on port ${port}`)
+})
